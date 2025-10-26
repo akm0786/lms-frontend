fe44e91128e84e0151064053cd6b395e72e381aa
Abhishek Mishra <akmakm10074@gmail.com>
2025-10-26 21:06:20 +0530

    added course description page

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Login from './Pages/Login';
 import CourseList from './Pages/Course/CourseList';
 import Contact from './Pages/Contact';
 import Denied from './Pages/Denied';
+import CourseDescription from './Pages/Course/CourseDescription';
 
 function App() {
 
@@ -23,6 +24,7 @@ function App() {
         <Route path="/denied" element={<Denied />} ></Route>
         <Route path="/contact" element={<Contact />} ></Route>
         <Route path="/courses" element={<CourseList />} ></Route>
+        <Route path="/course/description" element={<CourseDescription />} ></Route>
         <Route path="*" element={<NotFound />} ></Route>
 
       </Routes>
--- src/Components/CourseCard.jsx
+++ src/Components/CourseCard.jsx
@@ -1,17 +1,22 @@
 import { data } from "autoprefixer";
 import { useNavigate } from "react-router-dom"
 
-function CourseCard({data}) {
+function CourseCard({ data }) {
     const navigate = useNavigate();
 
     return (
-        <div 
-        onClick={() => navigate("/course/description/")}
-         className="text-white w-[22rem] h-[380px] p-2 shadow-lg rounded-lg coursor-pointer group overfow-hidden bg-zinc-700">
+        <div
+            onClick={() => navigate("/course/description/", { state: { ...data } })}
+            className="text-white w-[22rem] h-[380px] p-2 shadow-lg rounded-lg coursor-pointer group overfow-hidden bg-zinc-700">
             <div className="overflow-hidden ">
                 <img
                     className=" h-48 w-full rounded-tl-lg rounded-tr-lg group-hover:scale=[1,2] transition-all ease-in-out duration-300 "
-                    src={data?.thumbnail?.secure_url} alt="course thumbnail"
+                    src={data?.thumbnail?.secure_url}
+                    onError={(e) => {
+                        e.target.onError = null;
+                        e.target.src = "https://media.istockphoto.com/id/2173059563/vector/coming-soon-image-on-white-background-no-photo-available.jpg?s=612x612&w=0&k=20&c=v0a_B58wPFNDPULSiw_BmPyhSNCyrP_d17i2BPPyDTk=";
+                    }}
+                    alt="course thumbnail"
                 />
                 <div className="p-3 space-y-1 text-white">
                     <h2 className="text-xl font-bold line-clamp-2 text-yellow-500">{data?.title}</h2>
