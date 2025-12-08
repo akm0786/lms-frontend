ed2209bdb43770fcfc04ab3afd2299ad6539d820
Abhishek Mishra <akmakm10074@gmail.com>
2025-12-08 16:54:55 +0530

    added env and base_urls

--- src/Helpers/axiosInstance.js
+++ src/Helpers/axiosInstance.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/v1";
+// const BASE_URL = "http://localhost:5000/api/v1";
+const BASE_URL = import.meta.env.VITE_SERVER_URL;
 
 const axiosInstance = axios.create();
 
